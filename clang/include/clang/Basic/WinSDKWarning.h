1844578a9a2e3a1ab304acb10d90ceed48243df7
gmh5225 <2315157@qq.com>
2023-01-08 05:00:47 +0800

    [clang] Filter out 'win-sdk' warnings that are not needed.

---
+++ clang/include/clang/Basic/WinSDKWarning.h
@@ -0,0 +1,9 @@
+#ifndef LLVM_WINSDK_WARNING_H
+#define LLVM_WINSDK_WARNING_H
+#include <string>
+#include <vector>
+
+const std::vector<std::string> WinSDKWarningList = {
+    R"(Windows Kits\10\Include)", R"(Program Files (x86)\Windows Kits)"};
+
+#endif
