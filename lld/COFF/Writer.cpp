c357dbcadf4da4f600eae6a163d6373d80eeaa5b
gmh5225 <2315157@qq.com>
2023-06-28 12:53:08 +0800

    Update Writer.cpp

--- lld/COFF/Writer.cpp
+++ lld/COFF/Writer.cpp
@@ -916,9 +916,8 @@ void Writer::createSections() {
   auto createSection = [&](StringRef name, uint32_t outChars) {
     // If the user specified /driver, then we need to set the nonpaged attribute
     // for the specific sections.
-    if (ctx.config.driver &&
-        (name == ".text" || name == ".data" || name == ".rdata" ||
-         name == ".pdata" || name == ".00cfg"))
+    if (ctx.config.driver && (name == ".text" || name == ".data" ||
+                              name == ".rdata" || name == ".pdata"))
       outChars |= nonpaged;
     OutputSection *&sec = sections[{name, outChars}];
     if (!sec) {
