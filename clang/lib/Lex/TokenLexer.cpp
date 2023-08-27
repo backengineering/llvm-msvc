95b18058373c216ac3b0a227584cf6a56d4e711f
gmh5225 <2315157@qq.com>
2023-08-27 23:12:10 +0800

    Closes https://github.com/backengineering/llvm-msvc/issues/14

--- clang/lib/Lex/TokenLexer.cpp
+++ clang/lib/Lex/TokenLexer.cpp
@@ -822,6 +822,11 @@ bool TokenLexer::pasteTokens(Token &LHSTok, ArrayRef<Token> TokenStream,
       // Returns true the caller should immediately return the token.
       return true;
     }
+    // [MSVC Compatibility] Handle "__FUNCTION__"##"string"
+    if (LHSTok.is(tok::kw___FUNCTION__) && RHS.is(tok::string_literal)) {
+      // Returns true the caller should immediately return the token.
+      return true;
+    }
     // [MSVC Compatibility] Handle "string"##"string"
     if ((LHSTok.is(tok::string_literal) && RHS.is(tok::string_literal))) {
       // Returns true the caller should immediately return the token.
