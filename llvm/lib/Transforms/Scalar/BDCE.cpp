dea5baf841dad139d0ad059a0549a25bbb334dd7
gmh5225 <2315157@qq.com>
2023-09-09 02:02:14 +0800

    [Transforms][Scalar] Delete unused code

--- llvm/lib/Transforms/Scalar/BDCE.cpp
+++ llvm/lib/Transforms/Scalar/BDCE.cpp
@@ -97,9 +97,6 @@ static bool bitTrackingDCE(Function &F, DemandedBits &DB) {
     if (I.mayHaveSideEffects() && I.use_empty())
       continue;
 
-    if (I.isVolatile() && !I.isPHINodeOrSelectInstOrSwitchInst())
-      continue;
-
     // Remove instructions that are dead, either because they were not reached
     // during analysis or have no demanded bits.
     if (DB.isInstructionDead(&I) ||
