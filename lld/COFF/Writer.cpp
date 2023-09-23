27a0edcaca99dc73d2a1e3c1bd49470c81245749
gmh <13917777+gmh5225@users.noreply.github.com>
2023-09-23 17:42:57 +0800

    [LLD][COFF] Chang the value to 4 to support win-xp

--- lld/COFF/Writer.cpp
+++ lld/COFF/Writer.cpp
@@ -1523,10 +1523,12 @@ template <typename PEHeaderTy> void Writer::writeHeader() {
   pe->FileAlignment = config->fileAlign;
   pe->MajorImageVersion = config->majorImageVersion;
   pe->MinorImageVersion = config->minorImageVersion;
-  pe->MajorOperatingSystemVersion = config->majorOSVersion;
-  pe->MinorOperatingSystemVersion = config->minorOSVersion;
-  pe->MajorSubsystemVersion = config->majorSubsystemVersion;
-  pe->MinorSubsystemVersion = config->minorSubsystemVersion;
+  // Chang the value to 4 to support win-xp
+  pe->MajorOperatingSystemVersion = 4;
+  pe->MinorOperatingSystemVersion = 0;
+  // Chang the value to 4 to support win-xp
+  pe->MajorSubsystemVersion = 4;
+  pe->MinorSubsystemVersion = 0;
   pe->Subsystem = config->subsystem;
   pe->SizeOfImage = sizeOfImage;
   pe->SizeOfHeaders = sizeOfHeaders;
