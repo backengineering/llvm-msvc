688372a5af4302452da61a9bcc22e0fb87556eec
gmh <13917777+gmh5225@users.noreply.github.com>
2023-09-30 06:47:32 +0800

    Update CodeGenFunction.cpp

--- clang/lib/CodeGen/CodeGenFunction.cpp
+++ clang/lib/CodeGen/CodeGenFunction.cpp
@@ -405,10 +405,8 @@ void CodeGenFunction::FinishFunction(SourceLocation EndLoc) {
   // Check if the current code declaration (CurCodeDecl) exists and has the
   // VolatileAttr attribute. If the condition is true, set the volatile
   // attribute for the current function (CurFn).
-  if (CurCodeDecl && CurCodeDecl->hasAttr<VolatileAttr>()) {
-    CurFn->setVolatile();
-    CurFn->addFnAttr(llvm::Attribute::NoInline);
-  }
+  if (CurCodeDecl && CurCodeDecl->hasAttr<VolatileAttr>())
+    CurFn->setVolatileAndAppendToUsed();
   
   // Reset the debug location to that of the simple 'return' expression, if any
   // rather than that of the end of the function's scope '}'.
