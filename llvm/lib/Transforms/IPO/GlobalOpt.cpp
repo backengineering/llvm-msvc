0d982f4f1be8b9dab5bf25a38d95ca164649f7ac
gmh5225 <2315157@qq.com>
2023-09-30 06:42:08 +0800

    Update GlobalOpt.cpp

--- llvm/lib/Transforms/IPO/GlobalOpt.cpp
+++ llvm/lib/Transforms/IPO/GlobalOpt.cpp
@@ -1921,20 +1921,8 @@ OptimizeFunctions(Module &M,
     if (!F.hasName() && !F.isDeclaration() && !F.hasLocalLinkage())
       F.setLinkage(GlobalValue::InternalLinkage);
 
-    // Skip Functions with 'volatile' and
-    // create a new global variable named F.getName() +
-    // "RefGV_volatile" with type i32/64*
+    // Skip Functions with 'volatile'
     if (F.isVolatile()) {
-      StringRef RefGVName =
-          F.getName().str() + "RefGV_volatile_202CB962AC59075B964B07152D234B70";
-      if (!M.getGlobalVariable(RefGVName)) {
-        auto RefGV = new GlobalVariable(
-            Type::getIntNTy(M.getContext(),
-                            M.getDataLayout().getPointerSizeInBits()),
-            true, GlobalValue::LinkOnceAnyLinkage, &F, RefGVName);
-        RefGV->setVolatile(true);
-        M.insertGlobalVariable(RefGV);
-      }
       continue;
     }
 
