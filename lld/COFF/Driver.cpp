1409658b3d49e0577f4b460bc654ded584c2114e
gmh <13917777+gmh5225@users.noreply.github.com>
2023-10-01 19:06:57 +0800

    Update Driver.cpp

--- lld/COFF/Driver.cpp
+++ lld/COFF/Driver.cpp
@@ -1734,6 +1734,9 @@ void LinkerDriver::linkerMain(ArrayRef<const char *> argsArr) {
 
     if (auto *arg = args.getLastArg(OPT_pdb_source_path))
       config->pdbSourcePath = arg->getValue();
+
+    if (args.hasArg(OPT_fullpdbpath))
+      config->enableFullPdbPath = true;
   }
 
   // Handle /pdbstripped
@@ -2345,16 +2348,18 @@ void LinkerDriver::linkerMain(ArrayRef<const char *> argsArr) {
     // The embedded PDB path should be the absolute path to the PDB if no
     // /pdbaltpath flag was passed.
     if (config->pdbAltPath.empty()) {
-      // config->pdbAltPath = config->pdbPath;
-
-      // It's important to make the path absolute and remove dots.  This path
-      // will eventually be written into the PE header, and certain Microsoft
-      // tools won't work correctly if these assumptions are not held.
-      // sys::fs::make_absolute(config->pdbAltPath);
-      // sys::path::remove_dots(config->pdbAltPath);
-      // This way below can hide our pdb path.
-      config->pdbAltPath =
-          sys::path::filename(config->pdbPath, sys::path::Style::windows);
+      if (config->enableFullPdbPath) {
+        config->pdbAltPath = config->pdbPath;
+        // It's important to make the path absolute and remove dots.  This path
+        // will eventually be written into the PE header, and certain Microsoft
+        // tools won't work correctly if these assumptions are not held.
+        sys::fs::make_absolute(config->pdbAltPath);
+        sys::path::remove_dots(config->pdbAltPath);
+      } else {
+        // This way below can hide our pdb path.
+        config->pdbAltPath =
+            sys::path::filename(config->pdbPath, sys::path::Style::windows);
+      }
     } else {
       // Don't do this earlier, so that ctx.OutputFile is ready.
       parsePDBAltPath();
