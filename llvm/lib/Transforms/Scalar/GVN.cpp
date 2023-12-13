db450dfd9fc309f014bbfcb7077d5fc03fa584ea
gmh <13917777+gmh5225@users.noreply.github.com>
2023-12-14 03:35:06 +0800

    Update GVN.cpp

--- llvm/lib/Transforms/Scalar/GVN.cpp
+++ llvm/lib/Transforms/Scalar/GVN.cpp
@@ -2326,6 +2326,8 @@ void GVNPass::ValueTable::eraseTranslateCacheEntry(
 // question.  This is fast because dominator tree queries consist of only
 // a few comparisons of DFS numbers.
 Value *GVNPass::findLeader(const BasicBlock *BB, uint32_t num) {
+  if (!BB)
+    return nullptr;
   LeaderTableEntry Vals = LeaderTable[num];
   if (!Vals.Val) return nullptr;
 
