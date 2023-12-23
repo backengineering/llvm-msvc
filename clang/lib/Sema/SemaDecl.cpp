26cc3c891babe6175a6de8abad76c3721aef495c
gmh5225 <2315157@qq.com>
2023-12-24 03:52:22 +0800

    Update SemaDecl.cpp

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -17694,19 +17694,16 @@ Sema::ActOnTag(Scope *S, unsigned TagSpec, TagUseKind TUK, SourceLocation KWLoc,
       // Use a better diagnostic if an elaborated-type-specifier
       // found the wrong kind of type on the first
       // (non-redeclaration) lookup.
-#ifndef _WIN32
       if ((TUK == TUK_Reference || TUK == TUK_Friend) &&
           !Previous.isForRedeclaration() &&
           (!dyn_cast<TypedefDecl>(PrevDecl) || !isClassCompatTagKind(Kind))) {
-#else
-      if (!dyn_cast<TypedefDecl>(PrevDecl) || !isClassCompatTagKind(Kind)) {
-#endif
+#ifndef _WIN32
         NonTagKind NTK = getNonTagTypeDeclKind(PrevDecl, Kind);
         Diag(NameLoc, diag::err_tag_reference_non_tag)
             << PrevDecl << NTK << llvm::to_underlying(Kind);
         Diag(PrevDecl->getLocation(), diag::note_declared_at);
         Invalid = true;
-
+#endif
       // Otherwise, only diagnose if the declaration is in scope.
       } else if (!isDeclInScope(DirectPrevDecl, SearchDC, S,
                                 SS.isNotEmpty() || isMemberSpecialization)) {
