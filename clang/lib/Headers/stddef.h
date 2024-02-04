b73a75a8c74353625d5f21df7a337d32e8b923ce
gmh5225 <2315157@qq.com>
2024-02-05 01:06:27 +0800

    Fix conditional inclusion of __stddef_offsetof.h

--- clang/lib/Headers/stddef.h
+++ clang/lib/Headers/stddef.h
@@ -108,10 +108,12 @@
 #undef __need_max_align_t
 #endif /* defined(__need_max_align_t) */
 
-//#if defined(__need_offsetof)
-//#include <__stddef_offsetof.h>
-//#undef __need_offsetof
-//#endif /* defined(__need_offsetof) */
+#ifndef _WIN32
+#if defined(__need_offsetof)
+#include <__stddef_offsetof.h>
+#undef __need_offsetof
+#endif /* defined(__need_offsetof) */
+#endif
 
 /* Some C libraries expect to see a wint_t here. Others (notably MinGW) will use
 __WINT_TYPE__ directly; accommodate both by requiring __need_wint_t */
