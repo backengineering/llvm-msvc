c894b61f2694ee424e428c27b3bf919475c2b093
gmh <13917777+gmh5225@users.noreply.github.com>
2024-02-12 14:46:11 +0800

    Update SanitizerArgs.cpp

--- clang/lib/Driver/SanitizerArgs.cpp
+++ clang/lib/Driver/SanitizerArgs.cpp
@@ -1264,18 +1264,7 @@ void SanitizerArgs::addArgs(const ToolChain &TC, const llvm::opt::ArgList &Args,
 
   if (CfiCrossDso)
     CmdArgs.push_back("-fsanitize-cfi-cross-dso");
-
-  if (Args.hasArg(options::OPT_fdisable_cfi_check_fail))
-    CmdArgs.push_back("-fdisable-cfi-check-fail");
-
-  if (Args.hasArg(options::OPT_fdisable_cfi_slow_path_check))
-    CmdArgs.push_back("-fdisable-cfi-slow-path-check");
-
-  if (Args.hasArg(options::OPT_fandroid_kernel_dev_mode)) {
-    CmdArgs.push_back("-fdisable-cfi-check-fail");
-    CmdArgs.push_back("-fdisable-cfi-slow-path-check");
-  }
-  
+    
   if (CfiICallGeneralizePointers)
     CmdArgs.push_back("-fsanitize-cfi-icall-generalize-pointers");
 
